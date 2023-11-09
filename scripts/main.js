5ec1be79785718d86844a89077644cc86e123bcc
danieldribeiro <daniel.dribeiro@hotmail.com>
2023-11-09 17:41:32 -0300

    Build the API connection

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,92 @@
-const options = {
-    key: '58eb68d178a44ab19e5225538230711',
-	method: 'GET',
-	headers: {
-        'X-RapidAPI-Key': '8a04b1a555mshc5e6037e7da46edp1a711ajsn200128871740',
-		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-	}
+const searchContainer = document.querySelector(".search-container");
+const input = document.querySelector(".search-input");
+const icons = document.querySelectorAll(".bi");
+
+input.addEventListener("focus", () => {
+    for (const i of icons) {
+        i.classList.add("search-focus");
+    }
+
+    input.addEventListener("blur", () => {
+        for (const i of icons) {
+        i.classList.remove("search-focus");
+        }
+    });
+});
+
+// Adiciona um ouvinte de tecla para a tecla "Enter"
+input.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault(); // Impede a submissão padrão do formulário
+        search();
+    }
+});
+
+function search() {
+    let city = input.value;
+    getAndDisplayWeather(city);
+    input.value = ''
+}
+
+const getAndDisplayWeather = async (city) => {
+    try {
+        const data = await getWeatherData(city);
+        displayResult(data);
+    } catch (error) {
+        console.error(error.message);
+    }
 };
 
-const url = `http://api.weatherapi.com/v1/forecast.json?key=${options.key}&q=Rio de Janeiro&days=10&aqi=yes`;
+const getWeatherData = async (city) => {
+    const apiKey = '93794c18194ddc939f782804dd07dee6';
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
 
-fetch(url)
-    .then(response => {
-        if(!response.ok){
-            throw new Error(`http error: status ${response.status}`)
+    const response = await fetch(weatherUrl);
+
+    if (!response.ok) {
+        throw new Error(`HTTP error: status ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+};
+
+const displayResult = (data) => {
+    const cityName = document.querySelector('.city-name');
+    const temperature = document.querySelector('.temperature')
+    cityName.innerText = data.name;
+    temperature.innerHTML = `${data.main.temp.toFixed(0)}<span class="degrees">º</span>`
+
+    async function getCountryName(countryCode) {
+        try {
+            const response = await fetch(`https://restcountries.com/v3.1/alpha/${countryCode}`);
+            if (!response.ok) {
+                throw new Error(`HTTP error: status ${response.status}`);
+            }
+        
+            const data = await response.json();
+            const countryName = data[0]?.name?.common || 'Nome não encontrado';
+        
+            return countryName;
+        } catch (error) {
+            console.error(error.message);
+            return null;
         }
-        return response.json()
-    })
-    .catch(error => {
-        alert(error.message)
-    })
-    .then(response => {
-        displayResult(response)
-    })
+    }
 
-function displayResult(location){
-    const body = document.querySelector('body')
-    
-    console.log(location.location.name)
+    const countryCode = data.sys.country
+    getCountryName(countryCode)
+        .then(countryName => {
+            const country = document.querySelector('.country-name')
+            country.innerText = countryName
+    });
+};
+
+// Adiciona um ouvinte de clique para o ícone de pesquisa
+for (const i of icons) {
+    if (i.classList.contains("bi-search")) {
+        i.addEventListener("click", () => {
+        search();
+        });
+    }
 }
