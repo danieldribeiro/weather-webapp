9c4c2ab29a77273ae5ef4c82870c7aafb47c093b
Daniel Ribeiro <daniel.dribeiro@hotmail.com>
2023-11-09 22:34:22 -0300

    Did the function to change de background if its night time and some other elements

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,27 @@
-const searchContainer = document.querySelector(".search-container");
-const input = document.querySelector(".search-input");
-const icons = document.querySelectorAll(".bi");
+const searchContainer = document.querySelector(".search-container")
+const input = document.querySelector(".search-input")
+const icons = document.querySelectorAll(".bi")
 
 input.addEventListener("focus", () => {
     for (const i of icons) {
-        i.classList.add("search-focus");
+        if(!i.classList.contains('bi-brightness-high-fill')){
+            i.classList.add("search-focus")
+        }
     }
 
     input.addEventListener("blur", () => {
         for (const i of icons) {
-        i.classList.remove("search-focus");
+        i.classList.remove("search-focus")
         }
-    });
-});
+    })
+})
 
-// Adiciona um ouvinte de tecla para a tecla "Enter"
 input.addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
-        e.preventDefault(); // Impede a submissão padrão do formulário
-        search();
+        e.preventDefault()
+        search()
     }
-});
+})
 
 function search() {
     let city = input.value;
@@ -40,6 +41,7 @@ const getAndDisplayWeather = async (city) => {
 const getWeatherData = async (city) => {
     const apiKey = '93794c18194ddc939f782804dd07dee6';
     const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
 
     const response = await fetch(weatherUrl);
 
@@ -52,10 +54,9 @@ const getWeatherData = async (city) => {
 };
 
 const displayResult = (data) => {
+    //City
     const cityName = document.querySelector('.city-name');
-    const temperature = document.querySelector('.temperature')
     cityName.innerText = data.name;
-    temperature.innerHTML = `${data.main.temp.toFixed(0)}<span class="degrees">º</span>`
 
     async function getCountryName(countryCode) {
         try {
@@ -74,19 +75,79 @@ const displayResult = (data) => {
         }
     }
 
+    //Country
     const countryCode = data.sys.country
     getCountryName(countryCode)
         .then(countryName => {
             const country = document.querySelector('.country-name')
             country.innerText = countryName
     });
-};
 
-// Adiciona um ouvinte de clique para o ícone de pesquisa
+    //Temperature
+    const temperature = document.querySelector('.temperature')
+    temperature.innerHTML = `${data.main.temp.toFixed(0)}<span class="degrees">ºC</span>`
+
+    //Temperature Min - Max
+    const temperatureMinMax = document.querySelector('.max-min')
+    temperatureMinMax.innerText = `Max.: ${data.main.temp_max.toFixed(0)}ºC Min.: ${data.main.temp_min.toFixed(0)}ºC`
+
+    //Feels Like
+    const feelsLike = document.querySelector('.feels-like')
+    feelsLike.innerText = `Feels Like: ${data.main.feels_like.toFixed(0)}ºC`
+
+    //Pressure
+    const pressure = document.querySelector('.pressure')
+    pressure.innerText = `Pressure: ${data.main.pressure}(hPa)`
+
+    //humidity
+    const humidity = document.querySelector('.humidity')
+    humidity.innerHTML = `Humidity: ${data.main.humidity}%`
+
+    // Visibility
+    const visibility = document.querySelector('.visibility')
+    let visibilityToKm = (data.visibility / 1000)
+    visibility.innerText = `Visibility: ${visibilityToKm}Km`
+
+    //Weather description
+    const weatherDescription = document.querySelector('.weather-description')
+    weatherDescription.innerText = `${data.weather[0].description}`
+
+    //hour
+    function convertUnixToDateFormat(unixTimestamp) {
+        const date = new Date(unixTimestamp * 1000);
+    
+        const options = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+        } 
+
+        if(date.getHours() >= 18){
+            changeBgNight()
+        }
+    
+        const formattedDate = date.toLocaleDateString('en-US', options);
+        return formattedDate
+    }
+    
+    const unixTimestamp = data.dt
+    
+    const hour = document.querySelector('.hour')
+    hour.innerText = convertUnixToDateFormat(unixTimestamp)
+
+}
+
 for (const i of icons) {
     if (i.classList.contains("bi-search")) {
         i.addEventListener("click", () => {
-        search();
+            search();
         });
     }
+}
+
+function changeBgNight(){
+    document.body.style.backgroundImage = 'url("../images/clear-night.jpg")'
 }
