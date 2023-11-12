16f898a3a80be99b39e121db7d2db6837b48f63b
Daniel Ribeiro <daniel.dribeiro@hotmail.com>
2023-11-12 09:26:45 -0300

    Fix: sunny bg

--- scripts/main.js
+++ scripts/main.js
@@ -233,7 +233,7 @@ function changeBg(data){
         case 'algumas nuvens':
         case 'nuvens dispersas':
             (currentHour >= 18 || currentHour <= 3)
-            ? document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1612863229236-60722f5ef8c9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")'
+            ? document.body.style.backgroundImage = 'url("https://raw.githubusercontent.com/danieldribeiro/weather-webapp/main/images/sunny.jpg")'
             : document.body.style.backgroundImage = 'url("../images/sunny.jpg")'
             break
         case 'chovendo':
